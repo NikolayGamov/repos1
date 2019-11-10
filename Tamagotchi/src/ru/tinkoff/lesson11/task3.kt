7f14c7e849b661a13c46633c211496986ca69d7d
NikolayGamov <gamoff1997@gmail.com>
2019-11-11 00:36:00 +0300

    task3

--- Tamagotchi/src/ru/tinkoff/lesson11/task3.kt
+++ Tamagotchi/src/ru/tinkoff/lesson11/task3.kt
@@ -1,39 +1,46 @@
 package ru.tinkoff.lesson11
 
+class Pack( val arrival: Int, val duration: Int )
+{
+    var finishTime = -1
+}
+
 fun main()
 {
-    val bufferSize = scan.nextInt()
+    var bufferSize = scan.nextInt()
     val numb = scan.nextInt()
     if( numb == 0 )
         return
 
-    var inQueue = 0
-    var delay = 0
-
-    var processTime = 0
-
-
-
+    val packs = ArrayList<Pack>()
     for( i in 1..numb )
+        packs.add(Pack(scan.nextInt(), scan.nextInt()))
+
+    val output = ArrayList<Int>()
+    var time = -1
+    val queue = ArrayList<Pack>()
+    var queueFinishTime = 0
+    for( pack in packs )
     {
-        if( inQueue == bufferSize - 1 )
-            println( -1 )
-        else if( inQueue == 0 && processTime == 0)
-        {
-            println( scan.nextInt() )
-            processTime = scan.nextInt()
+        if( pack.arrival > time )
+            time++
+
+        if( !queue.isEmpty() ) {
+            for (i in 0..queue.size - 1) {
+                if (queue[i].finishTime <= time && queue[i].finishTime != -1)
+                    queue.removeAt(i)
+            }
         }
-        else if( inQueue == 0 )
+        if( queue.size < bufferSize )
         {
-            println( processTime + scan.nextInt() )
-            delay += scan.nextInt()
+            queue.add(pack)
+            output.add(maxOf(queueFinishTime, pack.arrival))
+            queueFinishTime += pack.duration
+            pack.finishTime = queueFinishTime
         }
         else
-        {
-            println( processTime + delay )
-            delay += scan.nextInt()
-        }
-        if( delay > 0 )
-            --delay
+            output.add(-1)
     }
+    for( timeFinish in output)
+        println(timeFinish)
 }
