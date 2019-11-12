e988c04572a12488521d8d6a498a09052351909c
NikolayGamov <gamoff1997@gmail.com>
2019-11-13 00:08:54 +0300

    updtask4

--- Tamagotchi/src/ru/tinkoff/lesson12/task12.4.kt
+++ Tamagotchi/src/ru/tinkoff/lesson12/task12.4.kt
@@ -1,16 +1,6 @@
 package ru.tinkoff.lesson12
 
-fun maxElemLen(list: List<String>): Int
-{
-    var maxLen = 0
-    for( element in list ) {
-        if (element.length > maxLen)
-            maxLen = element.length
-    }
-    return maxLen
-}
-
 fun main()
 {
-    print(goodNightBabies.find{it.length == maxElemLen(goodNightBabies)})
+    print( goodNightBabies.find{ personage -> personage == goodNightBabies.maxBy {it.length} } )
 }
