6e3c697f5ee133bbc746db2fbb99249ea34bdb08
NikolayGamov <gamoff1997@gmail.com>
2019-11-21 02:19:10 +0300

    upd

--- Tamagotchi/src/ru/tinkoff/lesson15/refactor1.kt
+++ Tamagotchi/src/ru/tinkoff/lesson15/refactor1.kt
@@ -1,5 +1,5 @@
 package ru.tinkoff.lesson15
-//refactoring les 12, 5, 12, 2.6
+
 val goodNightBabies = listOf("Хрюша", "Степаша", "Филя", "Гуля") // тип элементов списка указывать не обязательно
 
 fun printPersonages(personages: List<String>)
